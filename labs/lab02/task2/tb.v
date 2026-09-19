8d6ebe3e77b397a36e52e56ad4d937f5f663e8fa
manit-kachhwaha <f20230751@pilani.bits-pilani.ac.in>
2026-09-19 17:44:41 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,24 +4,42 @@
 module tb;
 
   // TODO: declare the inputs and outputs
+  reg [2:0] t_sel;
+  wire [7:0] t_dout;
 
   // TODO: instantiate DUT here
+  lut #(.WIDTH(8), .DEPTH(8)) dut (
+  .sel  (t_sel),
+  .dout (t_dout)
+);
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+      $dumpvars(0, dut);
     end
   end
 
-  initial begin
     // TODO: apply different input combinations
-
+initial begin
+    // Read every address, 5 time units apart
+    t_sel = 3'd0;
+    #5 t_sel = 3'd1;
+    #5 t_sel = 3'd2;
+    #5 t_sel = 3'd3;
+    #5 t_sel = 3'd4;
+    #5 t_sel = 3'd5;
+    #5 t_sel = 3'd6;
+    #5 t_sel = 3'd7;
+    #5 $finish;
   end
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+ initial
+    $monitor($time, " sel=%d | dout=%d", t_sel, t_dout);
+  
+
+
 
 endmodule
